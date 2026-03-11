67c45b6e9e8a92852b7723abd057aeb1a8478c12
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2026-03-11 15:34:18 -0300

    add na estrutura e anotações

--- src/main/java/com/pi/apigenatvdcomplementares/models/CoordenadorCurso.java
+++ src/main/java/com/pi/apigenatvdcomplementares/models/CoordenadorCurso.java
@@ -2,34 +2,28 @@ package com.pi.apigenatvdcomplementares.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
-@Table(name = "coordenador_curso")
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_coordenadores_curso")
 public class CoordenadorCurso extends Auditable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID) // Gera automaticamente um id do tipo:
-                                                    // "550e8400-e29b-41d4-a716-446655440000"
-    private String id;
+  @ManyToOne(fetch = FetchType.LAZY) 
+  @JoinColumn(name = "coordenador_id", nullable = false)
+  private Usuario coordenadorId;
 
-    @ManyToOne(fetch = FetchType.LAZY) // MUITOS CoordenadorCurso → UM Usuario (PERMITE QUE UM USUÁRIO (COORDENADOR),
-                                       // COORDENE MAIS DE UM CURSO)
-    @JoinColumn(name = "coordenador_id", nullable = false)
-    private Usuario coordenador;
+  @ManyToOne(fetch = FetchType.LAZY) // 
+  @JoinColumn(name = "curso_id", nullable = false)
+  private Curso cursoId;
 
-    @OneToOne(fetch = FetchType.LAZY) // Cada curso tem apenas um coordenador.
-    @JoinColumn(name = "curso_id", nullable = false, unique = true) // Unique → um curso só pode aparecer uma vez
-    private Curso curso;
+
 
 }
