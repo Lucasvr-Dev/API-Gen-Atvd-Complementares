e88280290f66d4472d54a509963d1bd6a90ef5ec
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-12 11:01:54 -0300

    submissão repository feito

--- src/main/java/com/pi/apigenatvdcomplementares/repository/SubmissaoRepository.java
+++ src/main/java/com/pi/apigenatvdcomplementares/repository/SubmissaoRepository.java
@@ -1,5 +1,16 @@
 package com.pi.apigenatvdcomplementares.repository;
 
-public class SubmissaoRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pi.apigenatvdcomplementares.models.Submissao;
+
+@Repository
+public interface SubmissaoRepository extends JpaRepository<Submissao, Long> {
+    List<Submissao> findAllByAlunoMatricula(String matricula); // Método para encontrar todas as submissões de um aluno pela matrícula
+
+    boolean existsByAlunoMatricula(String matricula); // Método para verificar se existem submissões para um aluno com a matrícula fornecida
+
 }
