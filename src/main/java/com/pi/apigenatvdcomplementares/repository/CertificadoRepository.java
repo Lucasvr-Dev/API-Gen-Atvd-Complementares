4819dbc196fe310989f18d4aa56c1f2a8443efcb
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-12 11:01:34 -0300

    certificado repository feito

--- src/main/java/com/pi/apigenatvdcomplementares/repository/CertificadoRepository.java
+++ src/main/java/com/pi/apigenatvdcomplementares/repository/CertificadoRepository.java
@@ -1,5 +1,16 @@
 package com.pi.apigenatvdcomplementares.repository;
 
-public class CertificadoRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pi.apigenatvdcomplementares.models.Certificado;
+
+@Repository
+public interface CertificadoRepository extends JpaRepository<Certificado, Long> {
+
+    List<Certificado> findBySubmissaoId(Long submissaoId);
+
+    boolean existsBySubmissaoId(Long submissaoId);
 }
