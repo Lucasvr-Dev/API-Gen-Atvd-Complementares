24f744149c8fae0ec2eb826bee4a19bd1b21a3f7
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-12 10:38:57 -0300

    curso repository feito

--- src/main/java/com/pi/apigenatvdcomplementares/repository/CursoRepository.java
+++ src/main/java/com/pi/apigenatvdcomplementares/repository/CursoRepository.java
@@ -1,5 +1,17 @@
 package com.pi.apigenatvdcomplementares.repository;
 
-public class CursoRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pi.apigenatvdcomplementares.models.Curso;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    Optional<Curso> findByNome(String nome); // Método para encontrar um curso pelo nome
+
+    boolean existsByNome(String nome);
+
 }
