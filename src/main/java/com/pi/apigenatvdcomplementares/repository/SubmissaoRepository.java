25fc3e06a812b29c71d6f53e9536f030c3fb2330
Lucasvr-Dev <lucasvrr.dev@gmail.com>
2026-03-13 22:16:02 -0300

    update: adicionando app.properties ao .gitignore

--- src/main/java/com/pi/apigenatvdcomplementares/repository/SubmissaoRepository.java
+++ src/main/java/com/pi/apigenatvdcomplementares/repository/SubmissaoRepository.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pi.apigenatvdcomplementares.enums.StatusSubmissao;
 import com.pi.apigenatvdcomplementares.models.Submissao;
 
 @Repository
 public interface SubmissaoRepository extends JpaRepository<Submissao, Long> {
-    List<Submissao> findAllByAlunoMatricula(String matricula); // Método para encontrar todas as submissões de um aluno pela matrícula
+    List<Submissao> findAllByAlunoMatricula(String matricula); // Método para encontrar todas as submissões de um aluno
+                                                               // pela matrícula
 
-    boolean existsByAlunoMatricula(String matricula); // Método para verificar se existem submissões para um aluno com a matrícula fornecida
+    boolean existsByAlunoMatricula(String matricula); // Método para verificar se existem submissões para um aluno com a
+                                                      // matrícula fornecida
+
+    List<Submissao> findByStatus(StatusSubmissao status);
 
 }
